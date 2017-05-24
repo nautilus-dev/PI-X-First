a185c371815f9354618be0a6b476b8682b230c63
nautilus-dev <schledep@informatik.uni-freiburg.de>
2017-05-24 10:17:54 +0200

    changed data Access to class

--- data_import.py
+++ data_import.py
@@ -8,26 +8,52 @@ Created on Mon May 22 22:00:46 2017
 import sqlite3
 import pandas
 
-con = sqlite3.connect(":memory:")
-
-df = pandas.read_csv("./data/Users.csv")
-df.to_sql("Users", con, if_exists='replace', index=True)
-
-df = pandas.read_csv("./data/Tasks.csv")
-df.to_sql("Tasks", con, if_exists='replace', index=True)
-
-df = pandas.read_csv("./data/Worker_Agents.csv")
-df.to_sql("WorkerAgents", con, if_exists='replace', index=True)
-
-df = pandas.read_csv("./data/Game_Sessions.csv")
-df.to_sql("GameSessions", con, if_exists='replace', index=True)
-
-con.commit()
+class dataManager : 
+    def __init__ (self) :
+        self.con = sqlite3.connect(":memory:")
+        self.cursor = self.con.cursor()
 
 
 
+    def readInCSV(self) :
+        # TODO : rewrite this as from Excel?
+        df_Users = pandas.read_csv("./data/Users.csv")
+        df_Users.to_sql("Users", self.con, if_exists='replace', index=True)
+        
+        df_Tasks = pandas.read_csv("./data/Tasks.csv")
+        df_Tasks.to_sql("Tasks", self.con, if_exists='replace', index=True)
+    
+        df_WorkerAgents = pandas.read_csv("./data/Worker_Agents.csv")
+        df_WorkerAgents.to_sql("WorkerAgents", self.con, if_exists='replace',
+                               index=True)
+    
+        df_GameSessions = pandas.read_csv("./data/Game_Sessions.csv")
+        df_GameSessions.to_sql("GameSessions", self.con, if_exists='replace',
+                               index=True)
+    
+        self.con.commit()
 
-print("trying to retrieve tables...")
-cur = con.cursor()
-cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print(cur.fetchall())
+
+    def runCheck(self) :
+        print("trying to retrieve tables...")
+        self.cursor.execute("SELECT name FROM sqlite_master "
+                                + "WHERE type='table';")
+        # TODO : Add a check whether the tables are read in 
+        #                        correctly instead of printing them
+        print(self.cursor.fetchall())
+    
+    def getValuesAsPandasObject(self, query) :
+        return pandas.read_sql_query(query, self.con)
+    
+    def printValues(self, query) :
+        data = self.getValuesAsPandasObject(query)
+        print data.values
+
+
+
+
+dM = dataManager()
+dM.readInCSV()
+dM.runCheck()
+
+dM.printValues("SELECT * FROM WorkerAgents LIMIT 20")

68fb47c0ace676f58e1ab9225e1e2e655ae788a5
Moritz Hoffmann <scutze@scutze-mbp.localdomain>
2017-05-26 14:04:51 +0200

    test run for for pi-switch algorithm

--- pi-x-first.py
+++ pi-x-first.py
@@ -5,22 +5,38 @@ Created on Wed May 24 10:32:48 2017
 """
 
 from data_import import dataManager
+from decider_classes import piDecider, Trial
+
 
 def main() :
     # main goes here
-    dM = dataManager()
+    # dM = dataManager()
 
-        
     # checking if there is a return:
-    dM.printValues("SELECT * FROM Users LIMIT 20")
-    
-    
+    # dM.printValues("SELECT * FROM Users LIMIT 20")
+
     # Example of getting the reputation in Round 2 for each Worker
     # for USER ID: 00212664-6c71-4e24-b61d-c7776f7a7d99
     # and the Workers level
 
- 
+    # Example for generating dataset with algorithm using session id
+    sid = "e7e52776-2750-4cde-a5da-093ccb8feaf9"
+    # set the pi-switch parameter
+    pi = 1
 
+    pd = piDecider(pi)
+    trial = Trial(sid)
+
+    # iterate over days and return algorithm assignment for each day
+    print(str("-"*80))
+    print("Generating Assignments")
+    print("SID=%s\tPI=%d" % (sid, pi))
+    print(str("-"*80))
+    for d in range(1, 6) :
+
+        pd.decide(trial)
+        print("Round: %d\t%s" % (d, pd.assignments))
+        trial.new_day()
 
 if __name__ == "__main__" :
     main()

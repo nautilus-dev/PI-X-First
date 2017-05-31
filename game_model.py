5986370a6154d248c8a0a47ae578c1b8537500fe
nautilus-dev <schledep@informatik.uni-freiburg.de>
2017-05-31 14:49:41 +0200

    removed spam output

--- game_model.py
+++ game_model.py
@@ -55,7 +55,7 @@ class gameModel :
         self.round = 0
         self.numTasks = numTasks
         self.getTaskEffortUnits()
-        self.getAgentsProductivity() 
+        self.getAgentsProductivity()
         self.agentsBacklog = [0 for i in xrange(self.numAgents)]
         # constructing the backlog queue
         for i in range(0, self.numAgents) :
@@ -66,14 +66,14 @@ class gameModel :
         # TODO : rework that it samples from the database:
         # self.dM.getValuesAsPandasObject("SELECT * FROM Tasks")
         self.taskEffortUnits = [1 for i in xrange(self.numTasks)]
-    
+
     def getAgentsProductivity (self) :
         # TODO : rework that it samples from the database:
         self.agentsProductivity = [(10 + i) for i in xrange(self.numAgents)]
 
 
     def caclulcateReputation (self, successfulNessByWA, loadbyWA) :
-        # TODO:         
+        # TODO:
         # for each WA: use the load (normalized by the amount of total tasks)
         # and the whether it was successful with all tasks,
         # 1.0 reputation would be all tasks of a round got
@@ -81,26 +81,26 @@ class gameModel :
         pass
 
     def getEffortPerTask(self, task) :
-        # TODO : get the effort from the database  
-        print ("constant 1 effort")
-        return 100
+        # TODO : get the effort from the database
+        # print ("constant 1 effort")
+        return 1
 
     def executeGame(self, assignments) :
         """
         Does all the game playing based oon the assignments
         """
         # TODO: calulculate whether the tasts are successfully finished
-        
+
         # we have all the assignments ans the effort units, just do scalar substraction?
-        
-        # assignments is an list of lists of the tasks for each worker. 
-        
+
+        # assignments is an list of lists of the tasks for each worker.
+
         # TODO: ONLY FOR TESTING!!!!
         # for i in range(0, self.numAgents) :
         #    q = Queue.Queue()
         #    assignments[i] = q
-        
-        # for each worker 
+
+        # for each worker
             # load its backlog queue
             # fill this backlog queue with the new tasks
             # work in the backlog queue until the there is no more task fitting in the productivity
@@ -109,25 +109,25 @@ class gameModel :
         self.round += 1
 
         for i in range(0, self.numAgents) :
-            print ("current Round is: " , self.round)
+            #print ("current Round is: " , self.round)
             backlog = self.agentsBacklog[i]
             newTasks = assignments[i]
             waLeftCapacity = self.agentsProductivity[i]
             for b in range(0, len(newTasks)) :
                 backlog.put(newTasks[b])
-            print ("working assignments parsed")
-            print ("backlog size is: " , backlog.qsize())
-            print ("agent productivity is: " , waLeftCapacity)
+            #print ("working assignments parsed")
+            #print ("backlog size is: " , backlog.qsize())
+            #print ("agent productivity is: " , waLeftCapacity)
             elem = -1
             while backlog.qsize() > 0 :
                 elem = backlog.get()
-                print ("Current Element " , elem)
+                #print ("Current Element " , elem)
                 effort = self.getEffortPerTask(elem)
                 if effort <= waLeftCapacity :
                     self.numSuccessfulEffort[i] += effort
                 else :
                     break
-            print ("working done")
+            #print ("working done")
             # now the failed elements and add them back to some queue
             # TODO: Rework here. looks super ugly
             leftTasks = Queue.Queue()
@@ -139,6 +139,6 @@ class gameModel :
                 leftTasks.put(elem)
             # add back the tasks
             self.agentsBacklog[i] = leftTasks
-            
 
-        
+
+
